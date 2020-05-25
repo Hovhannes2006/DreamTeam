fef26bcc05f86373471debb035dbbe99015111a5
Hovhannes2006 <64148220+Hovhannes2006@users.noreply.github.com>
2020-05-25 15:12:02 +0400

    Add files via upload

---
+++ blocks/header.php
@@ -0,0 +1,31 @@
+<div class="container-fluid d-block d-lg-none p-2 Cont-1">
+  <div class="row justify-content-center">
+      <button class="btn btn-outline-light col-8">Log In</button>
+  </div>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light Cont">
+  <a class="navbar-brand header-brand" href="index.php">Dream Team</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item mr-2">
+        <a class="nav-link nv-l" href="#">Home</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link nv-l" href="#">About Us</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link nv-l" href="#">Store</a>
+      </li>
+      <li class="nav-item mr-2">
+        <a class="nav-link nv-l" href="#">Workout Pricing</a>
+      </li>
+      <li class="nav-item mr-2 my-auto d-none d-lg-block">
+        <button class="btn btn-outline-light">Log In</button>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php 
+        require "blocks/forcss.php"
+    ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Dream Team | Home Page</title>
+</head>
+<body>
+    <?php
+        require "blocks/header.php"
+    ?>
+
+
+
+    <?php 
+        require "blocks/forjs.php"
+    ?>
+</body>
+</html>
